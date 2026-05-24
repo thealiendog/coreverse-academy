a3ae793ad443c1806dabadca50403e2e2a6cf287
Carolina Manos <carolinamanos@Carolinas-MacBook-Pro.local>
2026-05-24 13:03:17 -0700

    Slow all guide voices to 0.85x speed via ElevenLabs voice_settings.speed

--- netlify/functions/nova-speak.js
+++ netlify/functions/nova-speak.js
@@ -31,6 +31,7 @@ exports.handler = async (event) => {
           similarity_boost: 0.75,
           style:            0.3,
           use_speaker_boost: true,
+          speed:            0.85,
         },
       }),
     });
